225b8e41910edcf7d2759579cb776d34c391cd42
yehor <tka4951@gmail.com>
2016-10-26 23:16:51 +0300

    grid system in ChatPage and refactor Roomlist

--- src/components/ChatPage/ChatPage.jsx
+++ src/components/ChatPage/ChatPage.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import RoomList from './RoomList';
 import PageHeader from 'react-bootstrap/lib/PageHeader';
+import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
 
 import './ChatPage.scss';
 
@@ -12,9 +14,14 @@ export default class ChatPage extends React.Component {
   render() {
     return (
       <div className='chat-page'>
-        <PageHeader>MrrrChat <small>Just choose a room</small></PageHeader>
-        <RoomList />
+        <h1>MrrrChat</h1>
+        <Row className="show-grid">
+          <Col xs={12} md={8}><code>&lt;{'Col xs={12} md={8}'} /&gt;</code></Col>
+          <Col xs={6} md={4}><RoomList /></Col>
+        </Row>
       </div>
     );
   }
 }
+
+// <PageHeader>MrrrChat <small>Just choose a room</small></PageHeader>
--- src/components/ChatPage/RoomList.jsx
+++ src/components/ChatPage/RoomList.jsx
@@ -4,12 +4,14 @@ import ListGroupItem from 'react-bootstrap/lib/ListGroupItem';
 import Badge from 'react-bootstrap/lib/Badge';
 import Panel from 'react-bootstrap/lib/Panel';
 
+import './RoomList.scss';
+
 export default class RoomList extends React.Component {
   render() {
     return (
-      <div>
+      <div className='room-list'>
         <Panel header='Room list' bsStyle='primary'>
-          <ListGroup>
+          <ListGroup fill>
             <ListGroupItem href="#">Link 1 <Badge>12</Badge></ListGroupItem>
             <ListGroupItem href="#">Link 2 <Badge>12</Badge></ListGroupItem>
             <ListGroupItem href="#">Link 3 <Badge>12</Badge></ListGroupItem>
