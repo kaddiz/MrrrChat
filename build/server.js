1215c9278cce8590fbd8786afd31e9205bfea056
yehor <tka4951@gmail.com>
2016-10-27 21:00:46 +0300

    new server

--- build/server.js
+++ build/server.js
@@ -1,60 +1,13 @@
-const express                  = require('express');
-const React                    = require('react');
-const ReactDom                 = require('react-dom/server');
-const { match, RouterContext } = require('react-router');
-const routes                   = require('../src/routes.js');
-const { Provider }             = require('react-redux');
-const configureStore           = require('../src/redux/configureStore.js');
-
+const express  = require('express');
+const socket   = require('./socket.js');
+const path     = require('path');
 const app = express();
 
-const socket                   = require('./socket.js');
+app.use(express.static(__dirname));
 
-app.use((req, res) => {
-  const store = configureStore();
-
-  match({ routes, location: req.url },
-    (error, redirectLocation, renderProps) => {
-      if (redirectLocation) { // Если необходимо сделать redirect
-        return res.redirect(301, redirectLocation.pathname + redirectLocation.search);
-      }
-      if (error) { // Произошла ошибка любого рода
-        return res.status(500).send(error.message);
-      }
-      if (!renderProps) { // Мы не определили путь, который бы подошел для URL
-        return res.status(404).send('Not found');
-      }
-
-      const componentHTML = ReactDom.renderToString(
-        <Provider store={store}>
-          <RouterContext {...renderProps} />
-        </Provider>
-      );
-
-      return res.end(renderHTML(componentHTML));
-    }
-  );
-});
-
-function renderHTML(componentHTML) {
-  return `
-    <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MrrrChat</title>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="bundle.css">
-      </head>
-      <body>
-        <div id="root">${componentHTML}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-  `;
-}
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'index.html'))
+})
 
 const PORT = process.env.PORT || 3000;
 
