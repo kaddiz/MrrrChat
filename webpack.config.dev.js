3f7aa7e937142797672f65de1e3a55f3464c3603
yehor <tka4951@gmail.com>
2016-10-29 00:54:14 +0300

    Refactor webpack.config.* files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,10 +7,8 @@ module.exports = {
     extensions: ['', '.js', '.jsx']
   },
   debug: true,
-  // devtool: 'source-map',
   noInfo: true,
   entry: [
-    // 'webpack-hot-middleware/client?path=${window.location.protocol + "//" + window.location.host + "/"}/__webpack_hmr&reload=true',
     './src/index'
   ],
   output: {
@@ -19,32 +17,22 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('development') }),
     new webpack.optimize.OccurenceOrderPlugin(),
-    // new webpack.HotModuleReplacementPlugin(),
     new ExtractTextPlugin('bundle.css'),
     new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
-      {test: /(\.js$|\.jsx)$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file'},
-      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff"},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
-      {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
-      {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract('style', "css!postcss!sass")
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', "css!postcss")
-      }
-      // {test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract('style', 'css', 'postcss', 'sass') }
+      { test: /(\.js$|\.jsx)$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'file' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]' },
+      { test: /\.ico$/, loader: 'file?name=[name].[ext]' },
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract('style', "css!postcss!sass") },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style', "css!postcss") }
     ]
   },
   postcss: () => [autoprefixer]
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,17 +2,10 @@ const webpack           = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const autoprefixer      = require('autoprefixer')
 
-const GLOBALS = {
-  'process.env.NODE_ENV': JSON.stringify('production')
-};
-
 module.exports = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  // debug: true,
-  // devtool: 'source-map',
-  // noInfo: true,
   entry: './src/index',
   output: {
     path: `${__dirname}/build/`,
@@ -20,23 +13,22 @@ module.exports = {
     filename: 'bundle.js'
   },
   plugins: [
-    // new WebpackMd5Hash(),
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.DefinePlugin(GLOBALS),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
     new ExtractTextPlugin('bundle.css'),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({minimize: true, compress: { warnings: false }}),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, compress: { warnings: false } }),
   ],
   module: {
     loaders: [
-      {test: /(\.js$|\.jsx)$/, exclude: /node_modules/, loader: 'babel'},
-      {test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'url?name=[name].[ext]'},
-      {test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url?limit=10000&mimetype=application/font-woff&name=[name].[ext]"},
-      {test: /\.ttf(\?v=\d+.\d+.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=[name].[ext]'},
-      {test: /\.svg(\?v=\d+.\d+.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml&name=[name].[ext]'},
-      {test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]'},
-      {test: /\.ico$/, loader: 'file?name=[name].[ext]'},
-      {test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss!sass?sourceMap')}
+      { test: /(\.js$|\.jsx)$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.eot(\?v=\d+.\d+.\d+)?$/, loader: 'url?name=[name].[ext]' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url?limit=10000&mimetype=application/font-woff&name=[name].[ext]' },
+      { test: /\.ttf(\?v=\d+.\d+.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=[name].[ext]' },
+      { test: /\.svg(\?v=\d+.\d+.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml&name=[name].[ext]' },
+      { test: /\.(jpe?g|png|gif)$/i, loader: 'file?name=[name].[ext]' },
+      { test: /\.ico$/, loader: 'file?name=[name].[ext]' },
+      { test: /(\.css|\.scss)$/, loader: ExtractTextPlugin.extract('css?sourceMap!postcss!sass?sourceMap') }
     ]
   },
   postcss: () => [autoprefixer]
